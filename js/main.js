4f6dc98d0812e7c53774d4a8e3b71fd867ddf8c6
red0r4nge <a.kurilenko00@gmail.com>
2022-10-06 17:11:06 +0300

    little changes to navigation and navbar in header

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//Меняющийся фон
 $(function () {
   bg_from = 1;
   bg_to = 3;
@@ -11,3 +12,36 @@ $(function () {
     $('.banner__container').addClass('bg' + bg_now);
   }, bg_interval * 1000);
 });
+
+//Корзина
+jQuery(($) => {
+
+  // Уменьшаем на 1
+  $(document).on("click", ".input-number__minus", function () {
+    let total = $(this).next();
+
+    if (total.val() > 1) {
+      total.val(+total.val() - 1);
+    }
+  }
+
+  );
+
+  // Увеличиваем на 1
+  $(document).on("click", ".input-number__plus", function () {
+    let total = $(this).prev();
+    total.val(+total.val() + 1);
+  }
+
+  );
+
+  $(document).on("input", ".input-number__input", function () {
+    if (this.value.match(/[^0-9]/g)) {
+      this.value = this.value.replace(/[^0-9]/g, "");
+    }
+  }
+
+  );
+}
+
+);
